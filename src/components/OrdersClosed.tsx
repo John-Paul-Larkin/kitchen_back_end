2efddcd562fbff53dcecbb1521111ae58f170179
john Paul Larkin <johnplarkin@gmail.com>
2023-03-11 16:21:09 +0000

    closed details on hover

--- src/components/OrdersClosed.tsx
+++ src/components/OrdersClosed.tsx
@@ -1,9 +1,9 @@
 import { motion } from "framer-motion";
+import { useState } from "react";
 import styles from "../styles/MainScreen.module.css";
+import Items from "./Items";
 
 export default function OrdersClosed({ order }: { order: OrderDetails }) {
-  const handleShowClosedOrder = () => {};
-
   const timeOrderClosed = new Date(order.timeClosed!).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" }).substring(0, 5);
   const timeOrderPlaced = new Date(order.timeOrderPlaced!).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" }).substring(0, 5);
   const diff = new Date(order.timeClosed! - order.timeOrderPlaced!).getMinutes();
@@ -12,15 +12,42 @@ export default function OrdersClosed({ order }: { order: OrderDetails }) {
   const animate = { opacity: 1, scale: 1, y: 0 };
   const transition = { duration: 0.6 };
 
+  const [isShowDetails, setIsShowDetails] = useState(false);
+
   return (
-    <motion.div initial={initial} animate={animate} transition={transition} className={styles["single-order-closed"]} onClick={handleShowClosedOrder}>
+    <motion.div
+      initial={initial}
+      animate={animate}
+      transition={transition}
+      className={styles["single-order-closed"]}
+      onHoverStart={() => setIsShowDetails(true)}
+      onHoverEnd={() => setIsShowDetails(false)}
+    >
       <span>
         <div> Table {order.tableNumber}</div>
         <div> {order.server}</div>
-        <div>{timeOrderPlaced}</div>
-        <div>{timeOrderClosed}</div>
+        <div>Placed - {timeOrderPlaced}</div>
+        <div>Closed - {timeOrderClosed}</div>
         <div>{diff}</div>
       </span>
+      {isShowDetails && (
+        <div className={styles["closed-order-hover"]}>
+          <div> Table {order.tableNumber}</div>
+          <div> {order.server}</div>
+          <div>Order placed at - {timeOrderPlaced}</div>
+          <div>Order closed at - {timeOrderClosed}</div>
+          <div>{diff}</div>
+          {order.orderItemDetails &&
+            order.orderItemDetails.map((item) => {
+              return (
+                <div key={item.itemId}>
+                  {" "}
+                  <Items item={item} />
+                </div>
+              );
+            })}
+        </div>
+      )}
     </motion.div>
   );
 }
